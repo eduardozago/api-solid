a5ef5b265b5b5d4f4659cfbcd7078343bbfe8b42
Eduardo Zago <zagoe22@gmail.com>
2024-10-28 08:25:57 -0300

    Test: Implement test environment configuration

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,18 @@
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johdoe@example.com',
+      password: '123456',
+    })
+
+    console.log(user.password_hash)
+  })
+})
